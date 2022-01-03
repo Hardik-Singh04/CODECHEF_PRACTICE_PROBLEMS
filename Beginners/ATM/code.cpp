8cf739f52f18c76aa5e4732a80878a8540cb37a9
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2022-01-03 21:23:34 +0530

    Update code.cpp

--- Beginners/ATM/code.cpp
+++ Beginners/ATM/code.cpp
@@ -1,1 +1,19 @@
-.
+#include<stdio.h>
+    
+int main()
+{   
+    int transaction;
+    float amount,balance;
+  	scanf("%d%f",&transaction,&amount);
+  	balance=amount-transaction-0.5;
+   	if(transaction+0.5>amount)
+   	{
+   		printf("%.2f",amount);		
+   	}
+   	else if(transaction%5!=0)
+   	    printf("%.2f",amount);
+   	else
+  	    printf("%.2f",balance);    
+  
+    return 0 ;
+}
