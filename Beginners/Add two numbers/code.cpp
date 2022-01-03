c3599869eae5b2ba4d0922d07d0e2e126ffa0e91
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2022-01-03 21:24:34 +0530

    Update code.cpp

--- Beginners/Add two numbers/code.cpp
+++ Beginners/Add two numbers/code.cpp
@@ -1,1 +1,20 @@
-.
+
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+int main() {
+
+	int t;
+	scanf("%d",&t);
+	while(t>0)
+	{
+	    int a=0,b=0;
+	    scanf("%d%d",&a,&b);
+	    printf("%d\n",(a+b));
+	    t--;
+	}
+
+
+	return 0;
+}
