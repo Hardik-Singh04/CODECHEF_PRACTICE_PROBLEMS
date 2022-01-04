8cdf3ed2f8b0e647d10a28ff3f23d5b25282d9c0
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2022-01-04 09:38:14 +0530

    Create code.cpp

---
+++ Beginners/Enormous Input Test/code.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+int main() 
+{
+    int t=0,k=0,count=0;
+    scanf("%d%d",&t,&k);
+    while(t>0)
+    {
+        int a=0;
+        scanf("%d",&a);
+        if(a%k==0)
+        {
+          count=count+1; 
+        }
+        t--;
+    }
+	
+	cout<<count;
+	return 0;
+}
