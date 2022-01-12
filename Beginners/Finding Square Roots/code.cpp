0bbd5a334bfc0e703a99c5f4f2f11c56e4a4ff29
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2022-01-12 09:45:59 +0530

    Create code.cpp

---
+++ Beginners/Finding Square Roots/code.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+	int t=0;
+	cin>>t;
+	while(t--)
+	{
+	   int n=0,sq=0;
+	   cin>>n;
+	   sq=sqrt(n);
+	   cout<<sq<<endl;
+	}
+	return 0;
+}
