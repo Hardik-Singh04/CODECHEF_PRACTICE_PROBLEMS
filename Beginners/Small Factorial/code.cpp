07fe256fa1c36a2777776b2d4f6df0fcce41137c
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2022-02-08 10:39:11 +0530

    Update code.cpp

--- Beginners/Small Factorial/code.cpp
+++ Beginners/Small Factorial/code.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t=0;
+	cin>>t;
+	while(t--)
+	{
+	    int i,num=0,fact=1;
+	    cin>>num;
+	    
+	    for(i=1;i<=num;i++)
+	    {
+	        fact=fact*i;
+	    }
+
+	    cout<<fact<<endl;
+	}
+	return 0;
+}
 
