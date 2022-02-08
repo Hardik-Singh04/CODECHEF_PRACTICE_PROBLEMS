39b1c3c420201f19dad6675df66215d751e78578
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2022-02-08 10:37:53 +0530

    Update code.cpp

--- Beginners/The Lead Game/code.cpp
+++ Beginners/The Lead Game/code.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+using namespace std;
 
+int main() 
+{
+ int t, a, b, diff, max=0, winner, temp, p1=0, p2=0;
+ cin>>t;
+ while(t--)
+ {
+  cin>>a>>b;
+  p1=p1+a;
+  p2=p2+b;
+  if(p1>p2)
+  {
+   diff=p1-p2; 
+   temp=1;
+  }
+  else 
+  {
+   diff=p2-p1;
+   temp=2;
+  }
+  if(diff>max) 
+  {
+   max=diff; 
+   winner=temp;
+  }
+ }
+ cout<<winner<<" "<<max;
+ return 0;
+}
