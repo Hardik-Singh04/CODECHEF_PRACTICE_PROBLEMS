3b1d900d8aed3574a39f512b3c4d70430d166d42
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2022-03-16 15:18:54 +0530

    Create code.cpp

---
+++ Beginners/Helping Chef/code.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t=0;
+    cin>>t;
+    while(t--)
+    {
+        int n=0;
+        cin>>n;
+        if(n<10 )
+        {
+            cout<<"Thanks for helping Chef!"<<endl;
+        }
+        else
+        {
+            cout<<"-1"<<endl;
+        }
+    }
+	return 0;
+}
